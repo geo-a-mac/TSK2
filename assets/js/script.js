edcef3c9b65e12f42f9a65f8a810e531e13e68b8
George Mcalpine <georgemcalpine@Georges-iMac.local>
2022-03-30 16:42:34 -0400

    formatting task item

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,22 @@ var buttonEl = document.querySelector('#save-task');
 var tasksToDoEl = document.querySelector('#tasks-to-do');
 var formEl = document.querySelector('#task-form');
 
+
 var createTaskHandler = function(event) {
     event.preventDefault();
-
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
     var listItemEl = document.createElement('li');
-    listItemEl.textContent = 'New Task';
     listItemEl.className = 'task-item';
-    listToDoEl.appendChild(listItemEl);
+    
+    var taskInfoEl = document.createElement('div');
+    taskInfoEl.className = 'task-info';
+    taskInfoEl.innerHTML = "<h3 class ='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+    
+    tasksToDoEl.appendChild(listItemEl);
 }
 
 formEl.addEventListener("submit", createTaskHandler);
